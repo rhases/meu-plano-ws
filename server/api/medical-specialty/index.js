9feceb0390538eaacf009354803816631b8986ce
Tales Porto <talesap@gmail.com>
2016-12-09 17:55:15 -0200

    Criando a chamada para um procedimento e uma especialidade especifica.

--- server/api/medical-specialty/index.js
+++ server/api/medical-specialty/index.js
@@ -6,6 +6,7 @@ var controller = require('./medical-specialty.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/:id', controller.show);
 //router.post('/', controller.create);
 //router.put('/:id', controller.update);
 //router.patch('/:id', controller.update);
